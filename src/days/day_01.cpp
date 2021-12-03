2b7404e75302d8b5f3fae557bf23c38e2b9eee6a
Aspen Thompson <zebington@gmail.com>
2021-12-03 15:55:31 +0000

    Day 1

--- src/days/day_01.cpp
+++ src/days/day_01.cpp
@@ -4,10 +4,46 @@
 
 void aoc::day_01::part_one()
 {
-	std::cout << "Part One" << std::endl;
+	const auto lines = m_input.ints();
+
+	for (size_t i = 0; i < lines.size(); ++i)
+	{
+		const auto a = lines.at(i);
+		for (size_t j = i + 1; j < lines.size(); ++j)
+		{
+			const auto b = lines.at(j);
+			if (a + b == 2020)
+			{
+				std::cout << a * b << std::endl;
+				return;
+			}
+		}
+	}
 }
 
 void aoc::day_01::part_two()
 {
-	std::cout << "Part Two" << std::endl;
+	const auto lines = m_input.ints();
+
+	for (size_t i = 0; i < lines.size(); ++i)
+	{
+		const auto a = lines.at(i);
+		for (size_t j = i + 1; j < lines.size(); ++j)
+		{
+			const auto b = lines.at(j);
+			if (a + b >= 2020)
+			{
+				continue;
+			}
+			for (size_t k = j + 1; k < lines.size(); ++k)
+			{
+				const auto c = lines.at(k);
+				if (a + b + c == 2020)
+				{
+					std::cout << a * b * c << std::endl;
+					return;
+				}
+			}
+		}
+	}
 }
